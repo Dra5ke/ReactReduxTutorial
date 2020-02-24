9f89e216b458d83623b36b08918c19b647fc5e66
Stefan Harabagiu <sth@prooffice.dk>
2020-02-24 16:00:05 +0100

    lesson-10

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../../Calculator.css';
+import Display from '../Display';
+import ControlPanel from '../ControlPanel';
+import Digits from '../Digits';
+import Operators from '../Operators';
+import History from '../History';
+import store from '../../store';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <main className="react-calculator">
+                <Display text = {store.getState().curExpression}/>
+                <ControlPanel />
+                <Digits />
+                <Operators />
+                <History />
+            </main>
+        )
+    }
+}
+
+export default Calculator;
---
+++ src/components/Display/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Display = ({text = 0, clickHandler}) => (
+    <div className="display" onClick={clickHandler ? clickHandler : null}>
+        {text.toString().split(' ').reverse().join(' ')}
+    </div>
+);
+
+export default Display;
