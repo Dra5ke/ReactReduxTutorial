fd5781dc0148199a4e42cfbfa88bc95e30e4be51
Stefan Harabagiu <sth@prooffice.dk>
2020-02-28 10:07:02 +0100

    Lesson 13

--- src/actions/history/index.js
+++ src/actions/history/index.js
@@ -1,4 +1,5 @@
 import store from "../../store"
+import { EXPRESSION_UPDATE_ACTION, NEW_EXPRESSION_KEY } from "../expression";
 
 export const TOGGLE_HISTORY_ACTION = 'TOGGLE_HISTORY_ACTION';
 
@@ -10,6 +11,24 @@ export const showHistoryReducer = (state = false, { type }) => {
     switch(type) {
         case TOGGLE_HISTORY_ACTION: 
             return !state;
+        default:
+            return state;
+    }
+}
+
+export const isNotInHistory = (expression, history) => (
+    (expression || expression === 0) &&
+    (history.filter(i => i === expression.toString().trim()).length === 0)
+);
+
+export const historyReducer = (state = [], { type, payload }) => {
+    switch(type) {
+        case EXPRESSION_UPDATE_ACTION:
+            const expression = payload[NEW_EXPRESSION_KEY];
+            if(isNotInHistory(expression, state)) {
+                return [...state, expression.toString().trim()];
+            }
+            return state;
         default:
             return state;
     }
